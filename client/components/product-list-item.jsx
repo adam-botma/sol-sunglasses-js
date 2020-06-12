1217eb14cad3a5ced0d2aa9e1fd39a64c2989c8d
Adam Botma <adam.botma@me.com>
2020-06-12 23:41:57 +0000

    updates based on feedback

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,22 +3,19 @@ import React from 'react';
 class ProductListItem extends React.Component {
   constructor(props) {
     super(props);
-    this.name = props.name;
-    this.price = props.price;
-    this.shortDesc = props.shortDesc;
-    this.image = props.image;
+
   }
 
   render() {
     return (
       <div className="card">
         <div className="product-image-holder">
-          <img src={this.image} className="card-img-top small-product-image" alt=""/>
+          <img src={this.props.image} className="card-img-top small-product-image" alt=""/>
         </div>
         <div className="card-body">
-          <h4 className="card-title product-name">{this.name}</h4>
-          <h5 className="card-text price">$ {this.price}</h5>
-          <p className="card-text description">{this.shortDesc}</p>
+          <h4 className="card-title product-name">{this.props.name}</h4>
+          <h5 className="card-text price">$ {this.props.price}</h5>
+          <p className="card-text description">{this.props.shortDesc}</p>
         </div>
       </div>
 
