3127291126f3d943c1f08532d1f50eb528e19aab
Adam Botma <adam.botma@me.com>
2020-06-15 19:08:34 +0000

    User Can View Cart Summary

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function CartSummaryItem(props) {
+  return (
+    <div className= 'card'>
+      <div className="d-flex card-body">
+        <div className="col-4">
+          <img className='card-img-top'src={props.image} alt=""/>
+        </div>
+        <div className="col-8 d-flex flex-column justify-content-center">
+          <h2>{props.name}</h2>
+          <h3 className='summary-price'>${(props.price / 100).toFixed(2)}</h3>
+          <p>{props.shortDesc}</p>
+        </div>
+      </div>
+    </div>
+  );
+
+}
+
+export default CartSummaryItem;
